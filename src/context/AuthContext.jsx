a1bf784ed84e17175eaa6643f13421a1915c03d1
KingTechFoundation <muhirehillary459@gmail.com>
2025-06-03 19:41:18 +0300

    added /

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -9,7 +9,7 @@ export function AuthProvider({ children }) {
   const login = async (username, password) => {
     try {
       const response = await axios.post(
-        `${import.meta.env.VITE_API_URL}/auth/login`,
+        `${import.meta.env.VITE_API_URL}/api/auth/login`,
         {
           username,
           password,
